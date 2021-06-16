adb114211aefa31c5d7a7a068a7e1b99207bdbda
francis-siva <fsivasithamparam@gmail.com>
2021-06-17 01:19:21 +0200

    Increase Opacity gradually for posters description

--- src/app/work-skill/work-skill.component.ts
+++ src/app/work-skill/work-skill.component.ts
@@ -9,10 +9,34 @@ import  { SKILLS } from './../mock-skills';
 })
 export class WorkSkillComponent implements OnInit {
   skills = SKILLS;
-
+  noteOpacity: number = 0.0;
   constructor() { }
 
-  ngOnInit(): void {
+  /**
+   * Increase opacity of posters description gradually
+   * @param increaseTo (value between 0.0 to 1.0)
+   */
+  async increaseOpacity(increaseTo: number): Promise<void> {
+    await sleep(3000);
+
+    while(this.noteOpacity <= 1.0) {
+      this.noteOpacity += increaseTo;
+      console.log("noteOpacity at " + this.noteOpacity);
+      await sleep(250);
+    }
   }
 
+  ngOnInit(): void {
+    this.increaseOpacity(0.1)
+  }
+
+}
+
+/**
+ * Make a code execution delay for a given number of milliseconds.
+ * @param ms delay time in milliseconds
+ * @returns
+ */
+const sleep = (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
