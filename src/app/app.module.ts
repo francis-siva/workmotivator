0a3690877b08d68c363c3a455f061ec22bf2fd52
francis-siva <fsivasithamparam@gmail.com>
2021-06-28 13:43:37 +0200

    Menu component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
 import { WorkSkillComponent } from './work-skill/work-skill.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppTopBarComponent,
     WorkSkillComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
